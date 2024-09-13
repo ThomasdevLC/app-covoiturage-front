50148c85c05fd9b68cee19294d4a8d528a4243a6
abdelac <abdelac@hotmail.fr>
2024-09-13 15:39:42 +0200

    add company vehicle employee service.ts

--- src/app/service/company-vehicle/employee/company-vehicle-employee.service.ts
+++ src/app/service/company-vehicle/employee/company-vehicle-employee.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { CompanyVehicle } from '../../../models/company-vehicle.model';
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../auth/auth.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class CompanyVehicleEmployeeService {
-  private apiUrl =
-    'http://localhost:8080/company-vehicles/status-and-booking-dates';
+  private apiURL = environment.apiURL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getVehiclesByStatusAndBookingDates(
     startTime?: string,
@@ -24,6 +26,15 @@ export class CompanyVehicleEmployeeService {
       params = params.set('endTime', endTime);
     }
 
-    return this.http.get<CompanyVehicle[]>(this.apiUrl, { params });
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<CompanyVehicle[]>(
+      `${this.apiURL}company-vehicles/status-and-booking-dates`,
+      { params, headers }
+    );
   }
 }
