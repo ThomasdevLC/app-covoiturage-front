958d9ba666c403db3481d9e46057f63b074be5ad
ThomasdevLC <thomas.lecam@gmail.com>
2024-09-24 22:37:24 +0200

    rideshare service + redirection nav

--- src/app/service/rideshare/rideshare.service.ts
+++ src/app/service/rideshare/rideshare.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { RideShare } from '../../models/rideshare.model';
+import { environment } from '../../../environments/environment';
+import { SecureApiService } from '../api/secure-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RideShareService {
-  private apiURL = 'http://localhost:8080/rideshares';
+  private apiURL = environment.apiURL;
   private ridesharesSubject = new BehaviorSubject<RideShare[]>([]);
   rideshares$ = this.ridesharesSubject.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private secureApiService: SecureApiService
+  ) {}
 
   getRideShares(
     departureCity?: string,
@@ -23,10 +28,15 @@ export class RideShareService {
     if (arrivalCity) params = params.set('arrivalCity', arrivalCity);
     if (departureTime) params = params.set('departureTime', departureTime);
 
-    return this.http.get<RideShare[]>(`${this.apiURL}/search`, { params });
+    return this.http.get<RideShare[]>(`${this.apiURL}rideshares/search`, {
+      params,
+      headers: this.secureApiService.getHeaders(),
+    });
   }
 
   getRideShareById(id: number): Observable<RideShare> {
-    return this.http.get<RideShare>(`${this.apiURL}/${id}`);
+    return this.http.get<RideShare>(`${this.apiURL}/${id}`, {
+      headers: this.secureApiService.getHeaders(),
+    });
   }
 }
