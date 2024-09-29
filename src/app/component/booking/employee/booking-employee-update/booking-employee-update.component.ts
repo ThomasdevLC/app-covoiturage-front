38cadaaebf60e14c3b296e82df836b7ab9f14dfc
Penon Mickael <penon.mickael@orange.fr>
2024-09-29 19:27:40 +0200

    testMethodes

--- src/app/component/booking/employee/booking-employee-update/booking-employee-update.component.ts
+++ src/app/component/booking/employee/booking-employee-update/booking-employee-update.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { CompanyVehicleAdminService } from '../../../../service/company-vehicle/admin/company-vehicle-admin.service';
+import { BookingEmployeeService } from '../../../../service/booking/employee/booking-employee.service';
+import { EmployeeService } from '../../../../service/employee/employee.service';
 
 @Component({
   selector: 'app-booking-employee-update',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './booking-employee-update.component.html',
   styleUrl: './booking-employee-update.component.css'
 })
 export class BookingEmployeeUpdateComponent {
 
+  constructor(
+    private route: ActivatedRoute,
+    private vehicleService: CompanyVehicleAdminService,
+    private bookingEmployeeService: BookingEmployeeService,
+    //private numEmployee: number,
+    private employeeService: EmployeeService
+  ) {}
+  //
+ngOnInit(){
+  console.log("on init booking update");
+}
+  //
+  deleteBooking(){
+    console.log("delete booking");
+  }
+  //
+  updateBooking(){
+    console.log("update booking");
+  }
 }
