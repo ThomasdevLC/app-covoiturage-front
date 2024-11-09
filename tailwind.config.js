960fff35cf5e04a1e32d8ef8439aa7a3d0c32196
ThomasdevLC <thomas.lecam@gmail.com>
2024-11-09 18:36:34 +0100

    primeNg install + config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,ts}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+  "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+  extend: {},
   },
   plugins: [],
-};
+  };
