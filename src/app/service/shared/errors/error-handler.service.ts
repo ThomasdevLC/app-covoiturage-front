33b7cf76079fc31aa33f5b2724498acbfaebf74b
ThomasdevLC <thomas.lecam@gmail.com>
2024-11-16 18:25:53 +0100

    refactor error handler service

--- src/app/service/shared/errors/error-handler.service.ts
+++ src/app/service/shared/errors/error-handler.service.ts
@@ -1,38 +1,43 @@
+// error-handler.service.ts
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs'; 
+import { Observable, of } from 'rxjs';
 import { ToastService } from '../toast/toast.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorHandlerService {
-
   constructor(private toastService: ToastService) {}
 
-  // Méthode pour gérer les erreurs HTTP
   handleError(error: HttpErrorResponse): Observable<any> {
     let errorMessage = 'Une erreur est survenue. Veuillez réessayer plus tard.';
+    const isCriticalError = [403, 500].includes(error.status);
 
     if (error.error instanceof ErrorEvent) {
-      errorMessage = `Erreur: ${error.error.message}`;
+      // Erreur côté client ou réseau
+      errorMessage = `Erreur : ${error.error.message}`;
+    } else if (error.status === 500) {
+      // Erreur interne du serveur
+      errorMessage = "Erreur interne du serveur. Veuillez contacter l'administrateur.";
     } else {
-      if (error.status === 400 || error.status === 401 || error.status === 403 || error.status === 404) {
-        errorMessage = error.error; // Message de validation ou logique métier renvoyé par le backend
-      } else if (error.status === 500) {
-        errorMessage = 'Erreur interne du serveur. Veuillez contacter l\'administrateur.';
-      }
+      // Autres erreurs du backend
+      errorMessage = error.error || errorMessage;
     }
 
     console.error('Error status:', error.status);
     console.error('Error body:', error.error);
 
-    this.toastService.showError(error.status, errorMessage);
+    if (isCriticalError) {
+      this.toastService.showError(error.status, errorMessage);
+    } else {
+      this.toastService.showWarn(errorMessage);
+    }
+
     return of({
       status: error.status,
-      message: errorMessage
+      message: errorMessage,
     });
   }
-
-
 }
+
