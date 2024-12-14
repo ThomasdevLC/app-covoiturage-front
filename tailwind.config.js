4428bacc67d1caa5c576293a8fdec815205490e1
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-14 18:06:53 +0100

    tailwind + css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{html,ts}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-
-
-// };
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
-  extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)', // La classe Tailwind `bg-primary` utilisera `--primary-color`
+        'primary-light': 'var(--primary-light-color)', // Pour `bg-primary-light`
+        secondary: 'var(--secondary-color)', // Pour `bg-secondary`
+        'secondary-light': 'var(--secondary-light-color)',
+        border: 'var(--border-color)',
+        background: 'var(--background-color)',
+        text: 'var(--text-color)',
+      },
+      fontFamily: {
+        sans: ['var(--font-family-base)', 'sans-serif'], // Pour `font-sans`
+      },
+    },
   },
-  plugins: [],
-  };
+}
