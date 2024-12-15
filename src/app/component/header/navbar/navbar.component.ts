6ecd4dcb310be9bd0e4b5b7c0be8ae52c266d927
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-15 09:51:08 +0100

    navbar + dropdown style + test lucide icon

--- src/app/component/header/navbar/navbar.component.ts
+++ src/app/component/header/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeeConnected } from '../../../models/employee/employee-connected.model';
-import { AuthService } from '../../../service/auth/auth.service';
+import { AuthService } from '../../../service/auth/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { DropdownComponent } from '../dropdown/dropdown.component';
@@ -33,7 +33,7 @@ export class NavbarComponent  {
   logout(): void {
     this.authService.logout();
     this.authService.initializeCurrentUser();
-    this.router.navigate(['/']); 
+    this.router.navigate(['/']);
 
   }
 }
