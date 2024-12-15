08d6e734dbe20933c7293b96ce4121fd4d881358
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-15 14:01:35 +0100

    confirm when cancel passsenger rideshare participation

--- src/app/component/rideshare/passenger/rideshare-passenger-reservation-details/rideshare-passenger-reservation-details.component.ts
+++ src/app/component/rideshare/passenger/rideshare-passenger-reservation-details/rideshare-passenger-reservation-details.component.ts
@@ -1,17 +1,16 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import { RideShare } from '../../../../models/rideshare/rideshare.model';
-import { ActivatedRoute, Router } from '@angular/router';
 import { RidesharePassengerService } from '../../../../service/rideshare/passenger/rideshare-passenger.service';
 import { CommonModule } from '@angular/common';
 import { DateFormatterPipe } from '../../../../shared/pipe/date-formatter/date-formatter.pipe';
 import { RideSharePassengerDetails } from '../../../../models/rideshare/passenger/rideshare-passenger-details.model';
 import { ErrorHandlerService } from '../../../../shared/errors/error-handler.service';
+import {ConfirmDialogComponent} from "../../../../shared/lib/confirm-dialog/confirm-dialog.component";
 
 
 @Component({
   selector: 'app-rideshare-passenger-reservation-details',
   standalone: true,
-  imports: [CommonModule, DateFormatterPipe],
+  imports: [CommonModule, DateFormatterPipe, ConfirmDialogComponent],
   templateUrl: './rideshare-passenger-reservation-details.component.html',
   styleUrl: './rideshare-passenger-reservation-details.component.css'
 })
@@ -21,10 +20,13 @@ export class RidesharePassengerReservationDetailsComponent {
   @Output() closeDialog: EventEmitter<void> = new EventEmitter();
   past = false;
 
+  isConfirmVisible: boolean = false;
+  confirmationMessage: string = 'Êtes-vous sûr de vouloir annuler votre participation ?';
+  confirmationConfirmText: string = 'Oui, annuler';
+  confirmationCancelText: string = 'Non, garder ma participation';
+
   constructor(
     private rideshareService: RidesharePassengerService,
-    private route: ActivatedRoute,
-    private router: Router,
     private errorHandlerService: ErrorHandlerService
   ) {}
 
@@ -46,7 +48,24 @@ export class RidesharePassengerReservationDetailsComponent {
       },
     });
   }
+  // Méthode pour afficher le dialogue de confirmation
+  showConfirmation(): void {
+    this.isConfirmVisible = true;
 
+  }
+
+  // Méthode appelée lorsque l'utilisateur confirme l'annulation
+  onConfirmCancel(): void {
+    this.closeDialog.emit();
+    this.onCancel();
+  }
+  onCancelCancel(): void {
+    this.isConfirmVisible = false;
+    this.closeDialog.emit();
+
+  }
+
+  // Méthode pour annuler la participation
   onCancel(): void {
     if (!this.rideshare || !this.rideshare.id) return;
 
@@ -60,5 +79,4 @@ export class RidesharePassengerReservationDetailsComponent {
     });
   }
 }
-
 
