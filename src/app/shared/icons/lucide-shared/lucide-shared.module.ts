63c1ca95ea1e4be1ef9d981461856f0b17edb7fd
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-20 08:58:22 +0100

    icons added

--- src/app/shared/icons/lucide-shared/lucide-shared.module.ts
+++ src/app/shared/icons/lucide-shared/lucide-shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { LucideAngularModule,
   MessageSquare, UserRound,UserRoundCog, Home, Search,CalendarSearch, UserRoundSearch,UserRoundPlus,
   Route,Car,CarFront, IdCard,UsersRound, PowerOff , Mail, ContactRound,Timer, MapPin, Smartphone, CalendarClock, MapPinPlus,
-CirclePlus,Filter,PlusSquare,Clipboard, ClipboardCopy,ClipboardPaste,CircleParking, AlignJustify, X, MailOpen } from 'lucide-angular';
+CirclePlus,Filter,PlusSquare,Clipboard, ClipboardCopy,ClipboardPaste,CircleParking, AlignJustify, X, MailOpen, Pen,Trash2 } from 'lucide-angular';
 
 @NgModule({
   imports: [
     LucideAngularModule.pick({
       MessageSquare, UserRound,UserRoundCog, Home,Search,CalendarSearch,UserRoundSearch,UserRoundPlus,
       Route,Car,CarFront, IdCard,UsersRound, PowerOff , Mail, ContactRound,Timer, MapPin, Smartphone, CalendarClock, MapPinPlus,
-    CirclePlus,Filter,PlusSquare,Clipboard,ClipboardCopy,ClipboardPaste,CircleParking, AlignJustify, X, MailOpen })
+    CirclePlus,Filter,PlusSquare,Clipboard,ClipboardCopy,ClipboardPaste,CircleParking, AlignJustify, X, MailOpen, Pen,Trash2 })
   ],
   exports: [
     LucideAngularModule
