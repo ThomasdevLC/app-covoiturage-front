63db1c09e7a3e5cc107dfc20a22a19bee930f5c6
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-21 18:32:59 +0100

    rideshare-passenger-searc-item style

--- src/app/component/rideshare/passenger/rideshare-passenger-search-item/rideshare-passenger-search-item.component.ts
+++ src/app/component/rideshare/passenger/rideshare-passenger-search-item/rideshare-passenger-search-item.component.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DateFormatterPipe } from '../../../../shared/pipe/date-formatter/date-formatter.pipe';
 import { RideSharePassengerList } from '../../../../models/rideshare/passenger/ridehare-passenger-list.model';
+import { BadgeModule } from 'primeng/badge';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-rideshare-passenger-search-item',
   standalone: true,
-  imports: [CommonModule,RouterModule, DateFormatterPipe],
+  imports: [CommonModule, RouterModule, DateFormatterPipe, BadgeModule, LucideAngularModule],
   templateUrl: './rideshare-passenger-search-item.component.html',
   styleUrl: './rideshare-passenger-search-item.component.css'
 })
