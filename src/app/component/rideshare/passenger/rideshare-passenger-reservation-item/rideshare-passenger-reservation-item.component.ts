38312ae94735f7fef15bbfa19d0b5a32982759ea
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-21 18:32:21 +0100

    rideshare-passenger-reservation-item style

--- src/app/component/rideshare/passenger/rideshare-passenger-reservation-item/rideshare-passenger-reservation-item.component.ts
+++ src/app/component/rideshare/passenger/rideshare-passenger-reservation-item/rideshare-passenger-reservation-item.component.ts
@@ -7,12 +7,15 @@ import {ButtonModule} from "primeng/button";
 import {
   RidesharePassengerReservationDetailsComponent
 } from "../rideshare-passenger-reservation-details/rideshare-passenger-reservation-details.component";
+import { LucideAngularModule } from 'lucide-angular';
+import { LucideSharedModule } from '../../../../shared/icons/lucide-shared/lucide-shared.module';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-rideshare-passenger-reservation-item',
   standalone: true,
-  imports: [DateFormatterPipe,RouterModule,  DialogModule,
-    ButtonModule, RidesharePassengerReservationDetailsComponent],
+  imports: [DateFormatterPipe, RouterModule, DialogModule,
+    ButtonModule, RidesharePassengerReservationDetailsComponent, LucideAngularModule, LucideSharedModule, BadgeModule],
   templateUrl: './rideshare-passenger-reservation-item.component.html',
   styleUrl: './rideshare-passenger-reservation-item.component.css'
 })
